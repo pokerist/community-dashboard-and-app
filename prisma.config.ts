b1c4b0bde4dbc7f40506abb776f626d2d5895fc8
Maraamxx <marammahmoudxx@gmail.com>
2025-11-23 15:32:38 +0200

    fixed prisma.config.ts error

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,13 +1,13 @@
-import 'dotenv/config';
-import { defineConfig, env } from "prisma/config";
+import * as dotenv from 'dotenv';
+dotenv.config();
 
-export default defineConfig({
+export default {
   schema: "prisma/schema.prisma",
   migrations: {
     path: "prisma/migrations",
   },
   engine: "classic",
   datasource: {
-    url: env("DATABASE_URL"),
+    url: process.env.DATABASE_URL,
   },
-});
+};
