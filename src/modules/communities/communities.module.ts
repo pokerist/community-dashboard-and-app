35c62bb197bdb4e268496501905fa65c4a5cd7e6
Ahmed Wasfy <ahmedaimnwasfy@gmail.com>
2026-03-04 23:33:43 +0200

    fix(communities): import AuthModule to resolve PermissionsGuard dependencies

--- src/modules/communities/communities.module.ts
+++ src/modules/communities/communities.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from '../../../prisma/prisma.module';
+import { AuthModule } from '../auth/auth.module';
 import { CommunitiesController } from './communities.controller';
 import { CommunitiesService } from './communities.service';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, AuthModule],
   controllers: [CommunitiesController],
   providers: [CommunitiesService],
   exports: [CommunitiesService],
